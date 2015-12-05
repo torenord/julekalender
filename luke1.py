d9b92f7c55d49feb7f9bf31950679f20808d4863
Tore Norderud <tore.norderud@gmail.com>
2015-12-05 17:30:09 +0100

    .

--- luke1.py
+++ luke1.py
@@ -1,4 +1,4 @@
 from sys import stdin
 from re import match
 
-print sum(1 for line in stdin.readlines() if match("^[a-z]{,3}[0-9]{2,8}[A-Z]{3,}\n$", line))
+print sum(1 for line in stdin if match("^[a-z]{,3}[0-9]{2,8}[A-Z]{3,}\n$", line))
