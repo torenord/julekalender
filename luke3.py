2cc398f5448287555a2166cbb49d781b9d9706a2
Tore Norderud <tore.norderud@gmail.com>
2015-12-22 22:04:14 +0100

    .

--- luke3.py
+++ luke3.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Luke 3
+
+# Programmererenes dag feires hvert år på den 256. dagen (eller
+# 2^8. dagen om du vil) i året. Hvor mange ganger fra og med år 1 til
+# og med år 2015 har dette forekommet på en fredag om en antar at
+# 1. januar år 1 var en fredag i, samt at dagens skuddårsregler og
+# kalendersystem har vært gjeldende i hele perioden?
+
+# Hint: Skuddår er forøvrig et år som er delelig på 4 og ikke 100 med
+# mindre det er delelig på 400
+
+# OBS: Er blitt gjort oppmerksom på et avvik i oppgaven. Svaret som er
+# lagt inn på oppgaven later til å anta at den første dagen i året er
+# den "nulte" dagen i året.
+
 from datetime import date, timedelta
 
 friday = date(1, 1, 1).weekday()
