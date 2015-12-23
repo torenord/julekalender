9ed59953502d2e738d320eb6a925d2e241306ac3
Tore Norderud <tore.norderud@gmail.com>
2015-12-23 19:22:50 +0100

    .

--- luke12.py
+++ luke12.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Luke 12
+
+# Finn summen av alle tall fra og med 1 og til og med 100 000 000 som
+# er har 7 som en divisor, og samtidig IKKE har 5 som en divisor.
+
 print sum(i for i in range(7, 100000000+1, 7) if i % 5 != 0)
