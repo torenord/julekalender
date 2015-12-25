04f7bd1e8d658167fbac49d1bbdc55e70bc356cd
Tore Norderud <tore.norderud@gmail.com>
2015-12-25 20:30:14 +0100

    .

---
+++ luke24.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Luke 24
+
+# Det er i dag julaften, og har besluttet å kjøre en ganske enkel
+# siste luke. Dagens oppgave er:
+
+# Hva er summen av alle positive heltall fra og med 1 til og med 100
+# 000 000 000
+
+# Vi takker alle som har vært med for deltagelsen, og God Jul! :)
+
+n = 100000000000
+print n*(n+1)/2
