9c5b155dfedf7c2fede26a57298429018eb42ecd
Tore Norderud <tore.norderud@gmail.com>
2015-12-25 21:34:16 +0100

    .

---
+++ fetch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+curl -s http://pastebin.com/raw/F8z0JWqa > luke1.in
+curl -s http://pastebin.com/raw/sJVZp7BH > luke2.in
+curl -s http://pastebin.com/raw/sGbqMyCa > luke5.in
+curl -s http://pastebin.com/raw/sJVZp7BH > luke10.in
+curl -s http://pastebin.com/raw/p1eVAM5H > luke11.in
+curl -s http://pastebin.com/raw/LJX9cNvA > luke21.in
